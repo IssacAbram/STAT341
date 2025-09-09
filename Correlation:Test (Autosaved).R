88335778d841ca3e18fa0ca52b1445c5a2e52c2b
IssacAbram <iabram@claflin.edu>
2025-09-09 16:26:33 -0400

    Add files via upload

---
+++ Correlation:Test (Autosaved).R
@@ -0,0 +1,28 @@
+#Author: Issac, Date: 09/04/2025,  Purpose: Test the correlation
+
+if(!require(devtools)) install.packages("devtools"); devtools::install_github("kassambara/ggpubr")
+
+# Load the library ggpubr
+library (ggpubr)
+
+#Load some dummy dataset
+my_data <- mtcars
+
+Print first few lines
+head(my_data)
+
+#Hypothesis: Weight of the car is inversely porportional to mpg of the car
+
+•ggscatter(my_data, x = "mpg", y = "wt", add = "reg.line", conf.int = TRUE, cor.coef = TRUE, cor.method = "pearson", xlab = "Miles/(US) gallon", ylab = "Weight (1000 lbs)")
+
+
+Pearson's product-moment correlation
+
+data:  my_data$wt and my_data$mpg
+t = -9.559, df = 30, p-value = 1.294e-10
+alternative hypothesis: true correlation is not equal to 0
+95 percent confidence interval:
+ -0.9338264 -0.7440872
+sample estimates:
+       cor 
+-0.8676594 
