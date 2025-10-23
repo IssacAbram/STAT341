5624c83968f453025c67688cb2963bda1c49123f
IssacAbram <iabram@claflin.edu>
2025-10-23 15:20:38 -0400

    Add files via upload

---
+++ T-test.R
@@ -0,0 +1,12 @@
+#Author Issac; Date 10/21/25; Purpose; Calculate T-test
+
+#Create some dummy dataset for 2 variables
+x = rnorm(10)
+y = rnorm(10)
+
+#Create a density plot
+pts = seq(-4.5,4.5,length=100)
+plot(pts,dt(pts,df=9),col='red',type='l')
+lines(density(x), col='green')
+lines(density(y), col='blue')
+ttest = t.test(x,y)
